dcc99c813cf5b74b02685cf6f88022e3b6d4d6fe
abdulhadikaryana <karyana.abdhadi@gmail.com>
2020-03-16 15:38:37 +0700

    commit kedua

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=admin_new_penggiat',
+    'username' => 'admin_pb',
+    'password' => 'd4y4ngsumb1',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
+];
